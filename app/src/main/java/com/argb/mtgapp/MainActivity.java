5ce7ed7c36d2a91eb24fe70963b309200b4f7051
Andre Barros <andre815@gmail.com>
2019-02-17 18:29:11 -0300

    Migrate to AndroidX

--- app/src/main/java/com/argb/mtgapp/MainActivity.java
+++ app/src/main/java/com/argb/mtgapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.argb.mtgapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.View;
 
 import com.facebook.login.LoginManager;
--- app/src/main/java/com/argb/mtgapp/SplashActivity.java
+++ app/src/main/java/com/argb/mtgapp/SplashActivity.java
@@ -3,7 +3,7 @@ package com.argb.mtgapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
