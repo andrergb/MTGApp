e3845228004d74d32c328a3e813c8d1a930de5cd
Andre Barros <andre815@gmail.com>
2019-03-09 10:40:35 -0300

    Add basic profile info (with Glide)

--- app/src/main/java/com/argb/mtgapp/userInfo/UserInfoActivity.java
+++ app/src/main/java/com/argb/mtgapp/userInfo/UserInfoActivity.java
@@ -1,6 +1,7 @@
 package com.argb.mtgapp.userInfo;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.argb.mtgapp.R;
 import com.argb.mtgapp.userInfo.adapters.ViewPagerAdapter;
@@ -8,8 +9,12 @@ import com.argb.mtgapp.userInfo.fragments.FourFragment;
 import com.argb.mtgapp.userInfo.fragments.OneFragment;
 import com.argb.mtgapp.userInfo.fragments.ThreeFragment;
 import com.argb.mtgapp.userInfo.fragments.TwoFragment;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -17,13 +22,17 @@ import androidx.viewpager.widget.ViewPager;
 
 public class UserInfoActivity extends AppCompatActivity {
 
+    private FirebaseUser mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
 
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+
         CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
-        collapsingToolbarLayout.setTitle("Title toolbar");
+        collapsingToolbarLayout.setTitle(mUser.getDisplayName());
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -33,6 +42,12 @@ public class UserInfoActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        Glide.with(this)
+                .load(mUser.getPhotoUrl())
+                .apply(RequestOptions.circleCropTransform())
+//                .placeholder(R.drawable.ic_placeholder)
+                .into((ImageView) findViewById(R.id.user_info_avatar));
     }
 
     private void setupViewPager(ViewPager viewPager) {
